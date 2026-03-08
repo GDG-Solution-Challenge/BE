24c6e39768a926f35a4b673f1f8f2b989b946456
minuk07 <lmw3164@naver.com>
2026-03-08 19:55:47 +0900

    feat#6: checklist entity 및 repository 설계

---
+++ src/main/java/com/example/mamatolmi/domain/checklist/entity/Checklist.java
@@ -0,0 +1,32 @@
+package com.example.mamatolmi.domain.checklist.entity;
+
+import com.example.mamatolmi.domain.checklist.enums.ChecklistType;
+import com.example.mamatolmi.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "checklist")
+public class Checklist extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ChecklistType type;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "is_early", nullable = false)
+    private Boolean isEarly;
+}
